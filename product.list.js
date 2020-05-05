9c9d451473364bb17a04d1fe05875993964a9dfd
Weinman <danyerra@gmail.com>
2020-05-05 09:54:18 +0200

    feat: Product list for database / mock database initialization

---
+++ product.list.js
@@ -0,0 +1,82 @@
+const prodList = [	{
+					id:'62226y5246SOM',
+					type:'BedBase',
+					prize:927.54,
+					sold:127,
+					img:'someURL/base64IMG',
+					description: 'Un lujo de Somier'
+					},
+					{
+					id:'1256267SOM',
+					type:'BedBase',
+					prize:800.35,
+					sold:235,
+					img:'someURL/base64IMG',
+					description: 'Un buen somier'
+					},
+					{
+					id:'12554324352SOM',
+					type:'BedBase',
+					prize:700,65,
+					sold:100,
+					img:'someURL/base64IMG',
+					description: 'Un somier ideal'					
+					},
+					{
+					id:'1256267SOM',
+					type:'BedBase',
+					prize:500.54,
+					sold:64,
+					img:'someURL/base64IMG',
+					description: 'Un somier de mala calidad'
+					},
+					{
+					id:'125623427SOM',
+					type:'BedBase',
+					prize:45.00,
+					sold:25
+					img:'someURL/base64IMG',
+					description: 'El rompe espaldas por excelencia'
+					},
+					{
+					id:'62226y5246COL',
+					type:'Mattress',
+					prize:927.54,
+					sold:126,
+					img:'someURL/base64IMG',
+					description: 'Un lujo de Colchón'
+					},
+					{
+					id:'1256267COL',
+					type:'Mattress',
+					prize:800.35,
+					sold:234,
+					img:'someURL/base64IMG',
+					description: 'Un buen colchón'
+					},
+					{
+					id:'12554324352COL',
+					type:'Mattress',
+					prize:700,65,
+					sold:99,
+					img:'someURL/base64IMG',
+					description: 'Un colchón ideal'					
+					},
+					{
+					id:'1256267COL',
+					type:'Mattress',
+					prize:500.54,
+					sold:63,
+					img:'someURL/base64IMG',
+					description: 'Un colchón de mala calidad'
+					},
+					{
+					id:'125623427COL',
+					type:'Mattress',
+					prize:45.00,
+					sold:24
+					img:'someURL/base64IMG',
+					description: 'El rompe espaldas por excelencia'
+					}];
+
+module.exports = prodList;
