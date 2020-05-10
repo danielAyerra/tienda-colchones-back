a524c7cb57d0cdac7ce01303bc5ab7a90de216ed
Weinman <danyerra@gmail.com>
2020-05-10 13:46:34 +0200

    fix: Corrections in data format

--- user.list.js
+++ user.list.js
@@ -1,12 +1,13 @@
-[	
-	{
-		"email": "admin@admin.com",
-		"pass": "pass",
-		"isAdmin": true
+const userList=
+	[	{
+		email: 'admin@admin.com',
+		pass: 'pass',
+		isAdmin: true
 	},
 	{
-		"email": "user@user.com",
-		"pass": "pass",
-		"isAdmin": false
-	}
-]
+		email: 'user@user.com',
+		pass: 'pass',
+		isAdmin: false
+	}];
+
+module.exports = userList;
